0f0e79bc05818d840c38dae4f467703bf8a11b0e
suleimanyerima <suolrima@gmail.com>
2016-01-13 13:02:39 +0000

    Update Cipher.java

--- src/com/csit/packages/mobilesurvey/Cipher.java
+++ src/com/csit/packages/mobilesurvey/Cipher.java
@@ -12,16 +12,16 @@ public class Cipher {
 	
 	public String make (){
 		
-		sequence=sequence.replace("1", "P");
-		sequence=sequence.replace("2", "R");
-		sequence=sequence.replace("3", "O");
-		sequence=sequence.replace("4", "J");
-		sequence=sequence.replace("5", "M");
-		sequence=sequence.replace("6", "E");
-		sequence=sequence.replace("7", "B");
-		sequence=sequence.replace("8", "I");
-		sequence=sequence.replace("9", "C");
-		sequence=sequence.replace("0", "Q");
+		sequence=sequence.replace("1", "c");
+		sequence=sequence.replace("2", "7");
+		sequence=sequence.replace("3", "4");
+		sequence=sequence.replace("4", "n");
+		sequence=sequence.replace("5", "g");
+		sequence=sequence.replace("6", "3");
+		sequence=sequence.replace("7", "t");
+		sequence=sequence.replace("8", "h");
+		sequence=sequence.replace("9", "i");
+		sequence=sequence.replace("0", "s");
 		
 		return sequence;
 	}
